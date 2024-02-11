57267fff2e404c3e2abcd3b001badf138dc5e41b
TejaRamesh <tejaramesh2000@gmail.com>
2024-02-11 12:48:11 +0530

    American guy pushed code

--- src/main/java/qaclickacademy/RestAPITest.java
+++ src/main/java/qaclickacademy/RestAPITest.java
@@ -10,6 +10,10 @@ public class RestAPITest {
 		System.out.println("post Jira");
 		System.out.println("post Jira 2");
 		System.out.println("post Jira 3");
+		System.out.println("post jira 4");
+		System.out.println("post jira 5");
+		System.out.println("post jira 6");
+		System.out.println("post jira 7");
 	}
 	@Test
 	public void deleteTwitter()
